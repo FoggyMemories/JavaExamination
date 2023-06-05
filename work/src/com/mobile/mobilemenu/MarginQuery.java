823cb7b94100063c080927842ddc87af9b15b8c9
FoggyMemories <diiioman@foxmail.com>
2023-06-05 12:44:19 +0800

    //套餐余量查询

--- work/src/com/mobile/mobilemenu/MarginQuery.java
+++ work/src/com/mobile/mobilemenu/MarginQuery.java
@@ -5,6 +5,7 @@ import com.mobile.data.CustomerInfo;
 import com.mobile.util.ReturnUtil;
 
 //套餐余量查询
+
 public class MarginQuery {
     public MarginQuery(CustomerInfo ci) {
         System.out.println("您的卡号是:" + ci.getCardNumber());
