d561b63c7aeb1870d59a5dbdf4548253ca5de8f3
FoggyMemories <diiioman@foxmail.com>
2023-06-05 12:44:02 +0800

    //退出此层,返回上一级

--- work/src/com/mobile/mobilemenu/Exit.java
+++ work/src/com/mobile/mobilemenu/Exit.java
@@ -1,8 +1,11 @@
 package com.mobile.mobilemenu;
 
 //退出此层,返回上一级
+
+import com.mobile.topmenu.WelcomeScreen;
+
 public class Exit {
     public Exit(){
-        System.out.println("exit");
+        new WelcomeScreen();
     }
 }
