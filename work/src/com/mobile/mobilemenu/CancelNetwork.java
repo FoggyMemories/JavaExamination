e8c03bb146e829fd92ca7441590bc53b256093fb
FoggyMemories <diiioman@foxmail.com>
2023-06-05 23:31:35 +0800

    //办理退网

--- work/src/com/mobile/mobilemenu/CancelNetwork.java
+++ work/src/com/mobile/mobilemenu/CancelNetwork.java
@@ -8,7 +8,9 @@ import com.mobile.util.ReturnUtil;
 public class CancelNetwork {
     public CancelNetwork(CustomerInfo ci) {
 
-        System.out.println("办理退网");
+        System.out.println("------办理退网------");
+        System.out.println("卡号:" + ci.getCardNumber() + "办理退网成功!");
+        System.out.println("谢谢使用!");
 
         //提示用户返回上一级
         new ReturnUtil(ci);
