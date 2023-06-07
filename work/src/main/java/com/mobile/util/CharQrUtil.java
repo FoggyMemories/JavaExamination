09e317a02989f2da503c474a234293e12fc2f03b
FoggyMemories <diiioman@foxmail.com>
2023-06-07 19:25:38 +0800

    //QR生成Util

--- work/src/main/java/com/mobile/util/CharQrUtil.java
+++ work/src/main/java/com/mobile/util/CharQrUtil.java
@@ -24,14 +24,13 @@ public class CharQrUtil {
         int height = 1; // 二维码高度
 
         // 定义二维码的参数
-        HashMap<EncodeHintType, java.io.Serializable> hints = new HashMap<EncodeHintType, java.io.Serializable>();
+        HashMap<EncodeHintType, java.io.Serializable> hints = new HashMap<>();
         hints.put(EncodeHintType.CHARACTER_SET, "utf-8");//编码方式
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);//纠错等级
 
         // 打印二维码
         try {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
-            //StringBuilder sb = new StringBuilder("");
             for (int j = 0; j < bitMatrix.getHeight(); j++) {
                 for (int i = 0; i < bitMatrix.getWidth(); i++) {
                     if (bitMatrix.get(i, j)) {
